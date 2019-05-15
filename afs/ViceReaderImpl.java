f7bb631190eb748bfb56b677e347e517cff3272a
DanielMiM <48840142+DanielMiM@users.noreply.github.com>
2019-05-15 10:59:58 +0200

    Update ViceReaderImpl.java

--- afs/ViceReaderImpl.java
+++ afs/ViceReaderImpl.java
@@ -3,15 +3,32 @@
 package afs;
 import java.rmi.*;
 import java.rmi.server.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 
 public class ViceReaderImpl extends UnicastRemoteObject implements ViceReader {
     private static final String AFSDir = "AFSDir/";
+    private RandomAccessFile f;
 
-    public ViceReaderImpl(String fileName /* añada los parámetros que requiera */)
-		    throws RemoteException {
+    public ViceReaderImpl(String fileName,String modo /* añada los parámetros que requiera */)
+		    throws RemoteException, FileNotFoundException {
+    	f = new RandomAccessFile(fileName, modo );	
     }
     public byte[] read(int tam) throws RemoteException {
-        return null;
+        //A medio hacer
+    	byte [] b = new byte[tam];
+    	int  c=0;
+    	try {
+			while(f.read()!=-1) {
+				b[c]=f.readByte();	
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        return b;
+    }
     }
     public void close() throws RemoteException {
         return;
